e600dc2fdbc4a83f812b63bc82f74580ecceb13a
m.nithish kumar <you@example.com>
2025-08-19 10:18:29 +0530

    sixth update

--- setup.py
+++ setup.py
@@ -4,4 +4,6 @@ setup(name="medical-chatbot-assistant",
       author="nithishkumar",
       author_email="mnithish1231234@gmail.com",
       packages=find_packages(),
-      install_requires=["langchain_huggingface","langchain_groq","langchain-community","langchain-core","flask","sentence-transformers","langchain_pinecone"])
+      install_requires=["langchain_huggingface","langchain_groq","langchain-community","langchain-core","flask","sentence-transformers==4.1.0","langchain_pinecone","torch==2.8.0"])
+
+
